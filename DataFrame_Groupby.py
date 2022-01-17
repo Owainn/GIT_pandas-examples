527e29931ba0c935c8c4ac6c5a7968e67981d734
LouisFourel <louis.fourel.dev@gmail.com>
2022-01-17 12:25:31 +0100

    Test SSH Connexion

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -59,7 +59,7 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-hamtaro = cloneDF(mergeRatings)
-hamtaro = hamtaro.groupby(['movie_id', 'title'])['rating'].agg(
+hamtaroo = cloneDF(mergeRatings)
+hamtaroo = hamtaroo.groupby(['movie_id', 'title'])['rating'].agg(
     {'COUNT': np.size, 'myAVG': lambda x: x.sum() / float(x.count())}).sort_values('COUNT', ascending=False)
-print('My info sorted: \n%s' % hamtaro[:15])
+print('My info sorted: \n%s' % hamtaroo[:15])
